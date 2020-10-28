8f3a7a43e5120df64964cead246d93a8969825bc
Gibson Han <gibsonxhan@gmail.com>
2020-10-28 08:33:36 -0700

    fix-styling

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css'
 
 const Footer = ({ }) => {
     return <div className="Footer__container">
-        <p>Footer</p>
+        Footer
     </div>
 }
 
--- src/component/Hero.js
+++ src/component/Hero.js
@@ -5,13 +5,11 @@ import './Hero.css'
 
 const Hero = () => {
     return <div className="Hero__container">
-        <div classname="Hero__img-container">
-            <img src={heroImg} alt="hero_img" />
-        </div>
+        <img className="Hero__background-img" src={heroImg} alt="hero_img" />
         <h3 className="Hero__login-nav"> Login </h3>
         <div className="Hero__main-content">
             <img className="Hero__main-logo" src={rapttrLogo} alt="rapptr_logo" />
-            <h1 className="Hero__main-text">App Design & Development Agency</h1>
+            <h1 className="Hero__main-h1">App Design & Development Agency</h1>
         </div>
     </div>
 }
